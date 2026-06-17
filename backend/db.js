f063ca991e6aa0e758f50b5d95c979acc4d0bcb3
Bainazar110705 <149820878+Bainazar110705@users.noreply.github.com>
2026-06-17 17:14:41 +0500

    Add files via upload

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const ssl = process.env.PGSSLMODE === 'disable' ? false : { rejectUnauthorized: false };
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
